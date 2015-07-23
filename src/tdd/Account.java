3b5119f5adee53792d77f13b46016367b5f9d871
dkrasot <C:\Users\i.dovgopol\AppData\Roaming\The Bat!>
2015-07-23 08:45:41 +0300

    19 TDD Account project

---
+++ src/tdd/Account.java
@@ -0,0 +1,48 @@
+package tdd;
+
+public class Account {
+
+    public static void main(String[] args) {
+        
+    }
+    
+    private int startingBalance = 0;
+    private int interestRate = 0;
+
+    public Account() {
+    }
+
+    public Account(int startingBalance, int interestRate) {
+        this.startingBalance = startingBalance;
+        this.interestRate = interestRate;
+    }
+    
+    public int getInterestRate(){
+        return interestRate;
+    }
+
+    public int getStartingBalance() {
+        return startingBalance;
+    }
+
+    public int getEndingBalance() {
+        return this.getStartingBalance() + this.getStartingBalance() * this.interestRate / 100;
+    }
+
+    public void deposit(int amount) {
+        this.startingBalance += amount;
+    }
+
+    public void withdraw(int amount) {
+        this.startingBalance -= amount;
+    }
+
+    public Account buildNextYearAccount() {               
+        return new Account(this.getEndingBalance(),this.getInterestRate());
+    }
+
+    public int nextYearBalance(int rate) {
+        return startingBalance * (1 + rate / 100);
+    }
+}
+
---
+++ test/tdd/AccountTest.java
@@ -0,0 +1,46 @@
+package tdd;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class AccountTest {
+
+    private Account newAccount() {
+        return new Account(10000, 10);
+    }
+
+    @Test
+    public void startingBalance() {
+        assertEquals(10000, newAccount().getStartingBalance());
+    }
+
+    @Test
+    public void interestRate() {
+        assertEquals(10, newAccount().getInterestRate());
+    }
+
+    @Test
+    public void endingBalance() {
+        assertEquals(11000, newAccount().getEndingBalance());
+    }
+
+    @Test
+    public void thisYearEndingBalanceEqualsNextYearStartingBalance() {
+        Account thisYear = newAccount();
+        assertEquals(thisYear.buildNextYearAccount().getStartingBalance(), thisYear.getEndingBalance());
+    }
+
+    @Test
+    public void thisYearIntRateEqualsNextYearIntRate() {
+        Account thisYear = newAccount();
+        assertEquals(thisYear.getInterestRate(), thisYear.buildNextYearAccount().getInterestRate());
+    }
+    
+    @Test
+    public void withdrawingFundsAtTheBeginningOfTheYear(){
+        Account acc = newAccount();
+        acc.withdraw(1000);
+        assertEquals(9000,acc.getStartingBalance());
+        assertEquals(9900,acc.getEndingBalance());
+    }
+}
